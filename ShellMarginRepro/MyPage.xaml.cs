567ae9da64a138db20b4ef5bac85988823733d92
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-09-26 21:23:18 +1000

    Created Reproduction of issue with scroll view in grid.

---
+++ ShellMarginRepro/MyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellMarginRepro
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
